bda11f3953e3973b68b0107adc043800fcd9c9e1
Anderson Dutra <128000302+AndersonDutra7@users.noreply.github.com>
2023-10-19 21:35:03 -0300

    19.10

--- Estoque_Django/admin.py
+++ Estoque_Django/admin.py
@@ -1,11 +1,28 @@
+# from django.contrib import admin
+# from .models import Products
+
+# class ProductsAdmin(admin.ModelAdmin):
+
+#     list_display = ['nome','price', 'size', 'in_stock']
+#     list_filter = ['in_stock']
+#     list_editable = ['size']
+#     search_fields = ['nome']
+
+# admin.site.register(Products, ProductsAdmin)
+
+
 from django.contrib import admin
-from .models import Products
+from .models import Products, Categories
+
 
 class ProductsAdmin(admin.ModelAdmin):
-
-    list_display = ['nome','price', 'size', 'in_stock']
+    list_display = ['name', 'price', 'in_stock']
     list_filter = ['in_stock']
-    list_editable = ['size']
-    search_fields = ['nome']
+    search_fields = ['name']
+
 
 admin.site.register(Products, ProductsAdmin)
+
+admin.site.register(Categories)
+
+
--- Estoque_Django/views.py
+++ Estoque_Django/views.py
@@ -9,3 +9,9 @@ def index(request):
     for produto in produtos:
         print(f'produto.nome + produto.price')
     return render(request, 'pages/index.html', {'produtos':produtos})
+
+def product_detail(request, id):
+    product = Products.objects.get(id=id)
+    return render(request, 'pages/product_detail.html', {'product' : product})
+
+
